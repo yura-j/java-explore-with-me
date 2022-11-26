c1e8adb4ad5d69b35cbeef20eeab095646d2ac56
_yura <javadevelope@yandex.ru>
2022-11-26 13:15:28 +0300

    style-fix

--- statistic/src/main/java/ru/practicum/ewm/StatisticApplication.java
+++ statistic/src/main/java/ru/practicum/ewm/StatisticApplication.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class StatisticApplication {
     public static void main(String[] args) {
