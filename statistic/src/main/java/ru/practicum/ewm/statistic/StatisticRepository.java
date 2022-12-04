2111e8cf13d155a12f7c0d942a662871eb9adac4
_yura <javadevelope@yandex.ru>
2022-12-05 01:36:40 +0300

    fix

--- statistic/src/main/java/ru/practicum/ewm/statistic/StatisticRepository.java
+++ statistic/src/main/java/ru/practicum/ewm/statistic/StatisticRepository.java
@@ -8,19 +8,19 @@ import java.util.List;
 
 public interface StatisticRepository extends JpaRepository<StatisticHitModel, Long> {
 
-    @Query("SELECT new ru.practicum.ewm.StatisticModel(MAX(s.app), s.uri, COUNT(DISTINCT s.ip)) "
+    @Query("SELECT new ru.practicum.ewm.statistic.StatisticModel(MAX(s.app), s.uri, COUNT(DISTINCT s.ip)) "
             + "FROM StatisticHitModel AS s where s.uri in :uris AND s.timestamp BETWEEN :startTime AND :endTime  GROUP BY s.uri")
     List<StatisticModel> countUniqueHitsByStartTimeAndEndTimeAndUrisIn(LocalDateTime startTime, LocalDateTime endTime, List<String> uris);
 
-    @Query("SELECT new ru.practicum.ewm.StatisticModel(MAX(s.app), s.uri, COUNT(DISTINCT s.ip)) "
+    @Query("SELECT new ru.practicum.ewm.statistic.StatisticModel(MAX(s.app), s.uri, COUNT(DISTINCT s.ip)) "
             + "FROM StatisticHitModel AS s where s.timestamp BETWEEN :startTime AND :endTime  GROUP BY s.uri")
     List<StatisticModel> countUniqueHitsByStartTimeAndEndTime(LocalDateTime startTime, LocalDateTime endTime);
 
-    @Query("SELECT new ru.practicum.ewm.StatisticModel(MAX(s.app), s.uri, COUNT(s.ip)) "
+    @Query("SELECT new ru.practicum.ewm.statistic.StatisticModel(MAX(s.app), s.uri, COUNT(s.ip)) "
             + "FROM StatisticHitModel AS s where s.uri in :uris AND s.timestamp BETWEEN :startTime AND :endTime  GROUP BY s.uri")
     List<StatisticModel> countHitsByStartTimeAndEndTimeAndUrisIn(LocalDateTime startTime, LocalDateTime endTime, List<String> uris);
 
-    @Query("SELECT new ru.practicum.ewm.StatisticModel(MAX(s.app), s.uri, COUNT(s.ip)) "
+    @Query("SELECT new ru.practicum.ewm.statistic.StatisticModel(MAX(s.app), s.uri, COUNT(s.ip)) "
             + "FROM StatisticHitModel AS s where s.timestamp BETWEEN :startTime AND :endTime  GROUP BY s.uri")
     List<StatisticModel> countHitsByStartTimeAndEndTime(LocalDateTime startTime, LocalDateTime endTime);
 }
